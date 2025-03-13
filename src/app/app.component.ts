65de634acd0ef16365f2edcc69c954a762377fce
chathuraruwantha-2002 <143559027+chathuraruwantha-2002@users.noreply.github.com>
2025-03-14 04:58:05 +0530

    find Ui done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { NavbarComponent } from "./common/navbar/navbar.component";
 import { HomepageComponent } from "./common/homepage/homepage.component";
 import { PatientNavbarComponent } from "./common/patient-navbar/patient-navbar.component";
 import { HomepagePatientComponent } from "./common/homepage-patient/homepage-patient.component";
+import { FindComponent } from "./common/find/find.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HomepageComponent, PatientNavbarComponent, HomepagePatientComponent],
+  imports: [RouterOutlet, NavbarComponent, HomepageComponent, PatientNavbarComponent, HomepagePatientComponent, FindComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
