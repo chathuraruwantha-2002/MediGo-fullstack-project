ca7b25c99cf0f32be4223047f72eae272f8d75fb
chathuraruwantha-2002 <143559027+chathuraruwantha-2002@users.noreply.github.com>
2025-03-13 17:28:17 +0530

    patient homepage frontend commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./common/navbar/navbar.component";
 import { HomepageComponent } from "./common/homepage/homepage.component";
 import { PatientNavbarComponent } from "./common/patient-navbar/patient-navbar.component";
+import { HomepagePatientComponent } from "./common/homepage-patient/homepage-patient.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HomepageComponent, PatientNavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, HomepageComponent, PatientNavbarComponent, HomepagePatientComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
