bd188d5637b5c091f780c2dbc925bb1526de4ac0
chathuraruwantha-2002 <143559027+chathuraruwantha-2002@users.noreply.github.com>
2025-03-25 15:55:37 +0530

    commit patient-homepage validations
    frontend side

--- Frontend/src/app/pages/homepage-patient/homepage-patient.component.ts
+++ Frontend/src/app/pages/homepage-patient/homepage-patient.component.ts
@@ -2,29 +2,47 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Patient } from '../../model/patient';
+import { CommonModule } from '@angular/common';
+import { PatientHealthData } from '../../model/patientHealthData';
 
 @Component({
   selector: 'app-homepage-patient',
-  imports: [],
   templateUrl: './homepage-patient.component.html',
-  styleUrl: './homepage-patient.component.css'
+  styleUrl: './homepage-patient.component.css',
+  imports: [CommonModule]
 })
 export class HomepagePatientComponent {
 
-  patientStats: Patient | undefined;
+  patientStats: PatientHealthData | undefined;
+
+  statusBloodPressure: string = 'Normal';
+  statusBloodSugar: string = 'Normal';
+  statusBloodCount: string = 'Normal';
+  statusBMI: string = 'Normal';
+  status: string = 'Normal';
+
 
   constructor(private http: HttpClient) {
     this.loadPatientData();
   }
 
   private loadPatientData() {
-    
-    this.http.get<Patient>('http://localhost:8080/patient/dashboard/get-health-stats/1').subscribe(data => {
+
+    this.http.get<PatientHealthData>('http://localhost:8080/patient/dashboard/get-health-stats/1').subscribe(data => {
       console.log(data);
       this.patientStats = data;
-    });
 
+      this.displayStatusColors();
+    });
   }
 
+  private displayStatusColors() {
+
+    if (this.patientStats?.bloodPressure != null) {
+      this.status =
+        this.patientStats.bloodPressure >= 160 ? "High" :
+        (this.patientStats.bloodPressure >= 120 ? "Medium" : "Low");
+    }
+  }
 
 }
