4daa06a9a070e065ec5367d9c2bd1bd5523f102a
chathuraruwantha-2002 <143559027+chathuraruwantha-2002@users.noreply.github.com>
2025-03-25 18:50:07 +0530

    commit patient homepage
    F.end validations implemented

--- Frontend/src/app/pages/homepage-patient/homepage-patient.component.ts
+++ Frontend/src/app/pages/homepage-patient/homepage-patient.component.ts
@@ -14,35 +14,61 @@ import { PatientHealthData } from '../../model/patientHealthData';
 export class HomepagePatientComponent {
 
   patientStats: PatientHealthData | undefined;
+  patient: Patient | undefined;
 
   statusBloodPressure: string = 'Normal';
   statusBloodSugar: string = 'Normal';
   statusBloodCount: string = 'Normal';
   statusBMI: string = 'Normal';
-  status: string = 'Normal';
 
 
   constructor(private http: HttpClient) {
+    this.loadPatientHealthStats();
     this.loadPatientData();
   }
 
-  private loadPatientData() {
+  private loadPatientHealthStats() {
 
     this.http.get<PatientHealthData>('http://localhost:8080/patient/dashboard/get-health-stats/1').subscribe(data => {
       console.log(data);
       this.patientStats = data;
-
+      
       this.displayStatusColors();
     });
   }
 
   private displayStatusColors() {
-
     if (this.patientStats?.bloodPressure != null) {
-      this.status =
+      this.statusBloodPressure =
         this.patientStats.bloodPressure >= 160 ? "High" :
-        (this.patientStats.bloodPressure >= 120 ? "Medium" : "Low");
+        (this.patientStats.bloodPressure >= 120 ? "Normal" : "Low");
+    }
+
+    if (this.patientStats?.bloodSugar != null) {
+      this.statusBloodSugar =
+        this.patientStats.bloodSugar > 180 ? "High" :
+        (this.patientStats.bloodSugar >= 120 ? "Normal" : "Low");
+    }
+
+    if (this.patientStats?.bmi != null) {
+      this.statusBMI =
+        this.patientStats.bmi >= 30 ? "High" :
+        (this.patientStats.bmi >= 25 ? "Normal" : "Low");
+    }
+
+    if (this.patientStats?.heartRate != null) {
+      this.statusBloodCount =
+        this.patientStats.heartRate >= 100 ? "High" :
+        (this.patientStats.heartRate >= 60 ? "Normal" : "Low");
     }
   }
+
+  private loadPatientData() {
+    this.http.get<Patient>('http://localhost:8080/patient/dashboard/get-user/1').subscribe(data => {
+      console.log(data);
+      this.patient = data;
+    });
+  }
+
 
 }
